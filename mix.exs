852f2f267cfe9860415132b40cec8b3890a13cd7
Quentin Bonaventure <q.bonaventure@protonmail.com>
2024-09-03 12:11:55 +0000

    Adds mnesia as a mix.exs extra application

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule UbiNadeoApi.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :mnesia],
       mod: {UbiNadeoApi.Application, []}
     ]
   end
